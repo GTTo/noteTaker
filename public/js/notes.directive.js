66c8c8eb68c919a65a17929f445a37159433712e
Cesar Bevia <cbevia@sistel.es>
2016-03-14 11:01:03 +0100

    Save notes and clean vars

--- public/js/notes.directive.js
+++ public/js/notes.directive.js
@@ -9,17 +9,16 @@
             scope: {},          //Isolated scope
             templateUrl: 'templates/notepad.html',
             link: function(scope, elem, attrs) {
-                //init
-                var editor = elem.find('#editor');
-                
-                scope.notes = notesFactory.getAll(); // load notes
-
-                editor.bind('keyup keydown', function() {
-                    scope.noteText = editor.text().trim();
-                });
-
+                //Clean
+                scope.restore = function() {
+                    scope.editMode = false;
+                    scope.index = -1;
+                    scope.noteText = '';
+                };
                 //Edit
                 scope.openEditor = function(index) {
+                    scope.editMode = true;
+
                     if (index !== undefined) {
                         scope.noteText = notesFactory.get(index).content;
                         scope.index = index;
@@ -27,6 +26,28 @@
                         scope.noteText = undefined;
                     }
                 };
+                //Save
+                scope.save = function() {
+                    if (scope.noteText !== '') {
+                        var note = {};
+
+                        note.title = scope.noteText.length > 10 ? scope.noteText.substring(0, 10) + '. . .' : scope.noteText;
+                        note.content = scope.noteText;
+                        note.id = scope.index != -1 ? scope.index : localStorage.length;
+                        scope.notes = notesFactory.put(note);
+                    }
+
+                    scope.restore();
+                };
+                //init
+                var editor = elem.find('#editor');
+
+                scope.restore();  // initialize our app controls
+                scope.notes = notesFactory.getAll(); // load notes
+
+                editor.bind('keyup keydown', function() {
+                    scope.noteText = editor.text().trim();
+                });
             },
         };
     };
